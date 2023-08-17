983d07349ba00036ee9d300184a85ff9f9f839d4
Gokul <gokuls2381@gmail.com>
2023-08-17 22:53:24 +0530

    Selenium CW Aug 17

---
+++ src/main/java/mandatoryHomeWork/selenium/SalesforceTestXpath.java
@@ -0,0 +1,44 @@
+package mandatoryHomeWork.selenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class SalesforceTestXpath {
+
+
+	@Test
+	public void salesforceTest() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		driver.navigate().to("https://login.salesforce.com");
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("hari.radhakrishnan@qeagle.com");
+		driver.findElement(By.xpath("//input[@name='pw']")).sendKeys("Leaf$1234");
+		driver.findElement(By.xpath("//input[@name='Login']")).click();
+
+		WebElement launcher = driver.findElement(By.xpath("//div[@class='slds-icon-waffle']"));
+		launcher.click();
+
+		WebElement sales = driver.findElement(By.xpath("//p[normalize-space()='Sales']"));
+		sales.click();
+		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+		WebElement acc =	driver.findElement(By.xpath("//span[@class='slds-truncate'][normalize-space()='Accounts']"));
+		jsExecutor.executeScript("arguments[0].click();", acc);
+
+		driver.findElement(By.xpath("//div[@title='New']")).click();
+		driver.findElement(By.xpath("//input[@name='Name']")).sendKeys("Gokul");
+		driver.findElement(By.xpath("//button[@name='SaveEdit']")).click();
+		driver.quit();
+	}
+}
