6584db0cb8a886b2377adcb258a0670f8c1e101f
Gokul <gokuls2381@gmail.com>
2023-09-22 18:48:38 +0530

    LCHW beauty

---
+++ src/main/java/mandatoryHoweWork/septAlgos/Sep22LCHW_KBeauty.java
@@ -0,0 +1,49 @@
+package mandatoryHoweWork.septAlgos;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+public class Sep22LCHW_KBeauty {
+	//ip : int, int
+	//op: int
+	//Logic: init beauty, convert the num to string
+	//1. Run a for loop from start to index length-k
+	//2. Take substring from i to i+k and convert to int
+	//3. if the num !=0 && the num/sub has no remainder, inc beauty by 1
+	// return beauty
+	//Time complexity - O[N2]
+
+	public int divisorSubstrings(int num, int k) {
+		int beauty=0;
+		String s = String.valueOf(num);
+
+		for(int i=0;i<=s.length()-k;i++) {
+			int sub = Integer.valueOf(s.substring(i,i+k));
+			if(sub!=0&&num%sub==0) {
+				beauty++;
+			}
+		}
+		return beauty;
+
+	}
+
+	@Test
+	public void test1() {
+		Assert.assertEquals(1, divisorSubstrings(631,1));
+	}
+
+	@Test
+	public void test2() {
+		Assert.assertEquals(2, divisorSubstrings(240,2));
+	}
+
+	@Test
+	public void test3() {
+		Assert.assertEquals(1, divisorSubstrings(50000,3));
+	}
+	@Test
+	public void test4() {
+		Assert.assertEquals(2, divisorSubstrings(430043,2));
+	}
+
+}
