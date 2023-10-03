0c70b3b992907a447ee0a488f0e093d2d01ed3bb
Gokul <gokuls2381@gmail.com>
2023-10-03 19:29:18 +0530

    LCHW Oct3

---
+++ src/main/java/mandatoryHomeWork/octWeek1Algo/LCHWOct3DestinationMap.java
@@ -0,0 +1,67 @@
+package mandatoryHomeWork.octWeek1Algo;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+public class LCHWOct3DestinationMap {
+
+	//ip: List<List<String>>
+	//op : String
+	//Logic : init two set
+	//1. Add all the start to one set and end to another set
+	//2. Run a for loop to check if there is any value present in key set which is not there in value set using add method, if yes return it
+	//return ""
+	//TC : O[N], SC: O[2N]
+
+	public String findDestination(List<List<String>> paths) {
+		Set<String> keys = new HashSet<>(); 
+		for (List<String> path : paths) {
+			keys.add(path.get(0)); 
+		}
+
+		for (List<String> path : paths) {
+			if (keys.add(path.get(1))) {
+				return path.get(1); 
+			}
+		}
+		return "";
+	}
+
+	@Test
+	public void test1() {
+		List<List<String>> al = new ArrayList<>();
+		al.add(Arrays.asList("London","New York"));
+		al.add(Arrays.asList("New York","Lima"));
+		al.add(Arrays.asList("Lima","San Jose"));
+		Assert.assertEquals("San Jose", findDestination(al));
+	}
+	@Test
+	public void test2() {
+		List<List<String>> al = new ArrayList<>();
+		al.add(Arrays.asList("LN","NY"));
+		al.add(Arrays.asList("NY","L"));
+		al.add(Arrays.asList("L","SJ"));
+		Assert.assertEquals("SJ", findDestination(al));
+	}
+
+	@Test
+	public void test3() {
+		List<List<String>> al = new ArrayList<>();
+		al.add(Arrays.asList("A","B"));
+		al.add(Arrays.asList("C","D"));
+		al.add(Arrays.asList("B","C"));
+		Assert.assertEquals("D", findDestination(al));
+	}
+
+	@Test
+	public void test4() {
+		List<List<String>> al = new ArrayList<>();
+		al.add(Arrays.asList("Tiruppur","Bengaluru"));
+		Assert.assertEquals("Bengaluru", findDestination(al));
+	}
+}
