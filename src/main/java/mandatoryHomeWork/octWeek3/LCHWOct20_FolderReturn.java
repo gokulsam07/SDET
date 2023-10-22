e35feaa52372100c3b24ddb2f2120c46cecc6c56
gokulsam07 <gokuls2381@gmail.com>
2023-10-22 08:27:58 +0530

    RFs

--- src/main/java/mandatoryHomeWork/octWeek3/LCHWOct20_FolderReturn.java
+++ src/main/java/mandatoryHomeWork/octWeek3/LCHWOct20_FolderReturn.java
@@ -1,5 +1,47 @@
 package mandatoryHomeWork.octWeek3;
 
-public class LCHWOct20_FolderReturn {
+import java.util.Arrays;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LCHWOct20_FolderReturn {
+	
+	//ip: String[]; op:int
+	//Logic : init size
+	//Iterate all the elements in str[]
+	//1. if it equls ./ -- no ops
+	//2. if it equls ../ and size gte 0, reduce size
+	//3. inc size in all other case
+	// return size
+	//TC O[N], SC O[k]
+	public int minOperations(String[] logs) {
+	      int size=0;
+	        for (String s : logs) {
+	            if (s.equals("./")) {
+	            } else if (s.equals("../")) {
+	                if (size>0){
+	                    size--;
+	                }
+	            } else {
+	                size++;
+	            }
+	        }
+	        return size;
+	    }
+	
+	@Test
+	public void test() {
+		Assert.assertEquals(2, minOperations(new String[] { "d1/","d2/","../","d21/","./" }));
+	}
+
+	@Test
+	public void test1() {
+		Assert.assertEquals(3, minOperations(new String[] { "d1/","d2/","./","d3/","../","d31/" }));
+	}
+
+	@Test
+	public void test2() {
+		Assert.assertEquals(0, minOperations(new String[] { "d1/","../","../","../" }));
+	}
 }
